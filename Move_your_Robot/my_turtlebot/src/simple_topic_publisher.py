8139045a7098b3630cdf9cf0e226ad9f9dd06e3b
Surya Vamsi <32434128+SURYAVAMSIPSN@users.noreply.github.com>
2020-08-24 15:22:16 -0700

    Update simple_topic_publisher.py

--- Move_your_Robot/my_turtlebot/src/simple_topic_publisher.py
+++ Move_your_Robot/my_turtlebot/src/simple_topic_publisher.py
@@ -5,8 +5,7 @@ import rospy
 rospy.init_node("McFries_Publisher")
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1) # name of topic
 rate = rospy.rate(2)
-#count = Int32() # datatype of count
-#count.data = 0 # value stored in count
+
 
 var = Twist() # Twist has linear x, y, z and angular x, y and z. 
 # All are float64. 
@@ -17,6 +16,4 @@ while not rospy.is_shutdown():
     var.linear.x = 0.5
     var.angular.z = 0.5 # assign linear and angular velocities to the x and z values. 
     pub.publish(var)
-    #pub.publish(count)
-    #count.data += 1 # increment at each iteration
     rate.sleep()
